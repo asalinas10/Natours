27fd8ccff5811662416bd0755ff35ea930eda585
Adam Salinas <a_salinas1996@yahoo.com>
2024-04-25 11:29:56 -0700

    implemented cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,6 +28,22 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1. GLOBAL MIDDLEWARES
+// Implement CORS
+// Allow for simple action (get, post)
+app.use(cors());
+// This gives universal access from any domain
+// the permission llok like: Access-Control-Allow-Origin *
+// To give access to one specific domain: api.natours.com, front-end natours.com
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com',
+//   }),
+// );
+
+// app.options() is similar to .get(), .pathc(), etc., allows for complex actions (put, patch, delete)
+app.options('*', cors()); // All routes
+// app.options('/api/vi/tours/:id'); For one route
+
 // Serving Static Files
 app.use(express.static(path.join(__dirname, 'public')));
 // Set security HTTP headers
